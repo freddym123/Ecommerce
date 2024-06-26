6e7ea2e92a89ba956df9dd9277ec9cbfe4f1cd26
Freddy Martinez <freddy234@gmail.com>
2024-06-25 18:22:59 -0700

    added base to apps

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig',
+    'base',
     'django_icons',
     'bootstrap5',
     'cart.apps.CartConfig',
